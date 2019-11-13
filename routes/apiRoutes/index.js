6daaa537ca9d850702a70ee4e3734a3703e615e1
Andrew Jolivette <amjolivette@gmail.com>
2019-11-13 12:05:32 -0800

    created routes and controllers for favorite songs and search history

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,11 +2,15 @@ const router = require('express').Router();
 
 const authRoutes = require('./authRoutes');
 const blogRoutes = require('./blogRoutes');
+const favSongRoutes = require('./favSongRoutes');
+const historyRoutes = require('./historyRoutes');
 
 // Every route inside of here
 // has /api
 router.use('/auth', authRoutes);
 router.use('/blogs', blogRoutes);
+router.use('/favsongs', favSongRoutes);
+router.use('/history', historyRoutes);
 
 
 module.exports = router;
