c7244656340aa2192a15fb8b44f71c5410c221a2
Peter Wu <23109224+PeterBWu@users.noreply.github.com>
2019-11-14 09:37:59 -0800

    changed link from dev.js to keys.js

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 const Spotify = require('node-spotify-api');
-const keys = require("./../config/dev")
+const keys = require("./../config/keys")
 
 // temporary search parameteres
 const artist = "";
