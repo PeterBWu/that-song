e7759a9f8801b7be38fb46fac594360dc5758114
Peter Wu <23109224+PeterBWu@users.noreply.github.com>
2019-11-14 08:54:08 -0800

    added other keys for heroku deployment

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,10 @@
 module.exports = {
-    secret: process.env.SECRET
+    secret: process.env.SECRET,    
+    musixmatchAPIkey: process.env.MUSIXMATCH,
+    youtubeAPIkey: process.env.YOUTUBE,
+    spotify: {
+      id: process.env.SPOTIFY_ID,
+      secret: process.env.SPOTIFY_SECRET
+    }
+
 };
