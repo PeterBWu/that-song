8f10513849508b7d3ea068c6689151add5773169
Paul Thomas <traitorpt@gmail.com>
2019-11-16 00:22:48 -0800

    adjusted landing page to have wider search bar

--- client/src/containers/LandingPage.js
+++ client/src/containers/LandingPage.js
@@ -18,18 +18,18 @@ class LandingPage extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <div id="input_box" style={{ float: "none" }}>
+      <div id="input_box" className="w-75 bg-dark" style={{ float: "none" }}>
         <form onSubmit={handleSubmit(this.onSubmit)}>
           <Field
             name="lyric"
             type="text"
             component="input"
             autoComplete="none"
-            className="p-2 search_input searchField"
+            className="p-2 sol-10 w-100 search_input searchField text-center"
             placeholder="Find That Song!!"
             aria-label="Search"
           />
-          <button className="fa fa-search btn btn-outline-secondary text-dark my-2 my-sm-0 p-2 searchClick" type="submit"></button>      </form>
+          <button className="col-2 float-right fa fa-search btn btn-outline-info my-2 my-sm-0 p-2 searchClick" type="submit"></button>      </form>
       </div>
     );
   }
