8aefdd7f8417e416f8f70a98993d4f8efcb431ab
Peter Wu <23109224+PeterBWu@users.noreply.github.com>
2019-11-16 20:30:26 -0800

    added styling to results/song details

--- client/src/components/InfoCard.js
+++ client/src/components/InfoCard.js
@@ -6,13 +6,20 @@ const InfoCard = props => {
   const genre = song.primary_genres.music_genre_list.length >0?song.primary_genres.music_genre_list[0].music_genre.music_genre_name : "N/A" 
   console.log(genre)
   return(
-      <div id={song.track_id}>
-        <Link to={`/results/${song.track_id}/${song.artist_name}/${song.track_name}`}>{song.track_id}</Link>
+      <div id={song.track_id} className="card m-1">
+        <div className='card-body'>
+
         {/* <p>{JSON.stringify(song)}</p> */} 
-        <h3><span className="bolded">Track Name:</span> {song.track_name}</h3>
-        <p><span className="bolded">Artist:</span> {song.artist_name}</p>
-        <p><span className="bolded">Album:</span> {song.album_name}</p>
-        <p><span className="bolded">Genre:</span> {genre}</p>
+        <h5 className='card-title'><span className="bolded">Track Name:</span> {song.track_name}</h5>
+        <h6 className='card-subtitle'><span className="bolded">Artist:</span> {song.artist_name}</h6>
+        
+        <p className='card-text'><span className="bolded">Album:</span> {song.album_name}
+        <br/>
+        <span className="bolded">Genre:</span> {genre}</p>
+
+        
+        <Link to={`/results/${song.track_id}/${song.artist_name}/${song.track_name}`} className='card-link'>Learn More!</Link>
+        </div>
       </div>
     )
 
